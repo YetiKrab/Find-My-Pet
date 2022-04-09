00de1e710f040c4d25d1bf89292693605866eb8d
Celeste Knopf <Celeste@Celestes-Air.home>
2022-04-09 12:30:40 -0400

    routes and server

---
+++ server/routes/petRoute.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const petController = require('../controller/petController');
+
+const router = express.Router();
+
+// data request route handler
+router.get('/', petController.getPosts, (req, res) => {
+  return res.status(200).json(res.locals.posts);
+});
+
+// posting route handler
+router.post('/', petController.addPost, petController.getPosts, (req, res) => {
+  return res.status(201).json(res.locals.posts);
+});
+
+// deleting route handler
+router.delete('/', petController.deletePost, (req, res) => {
+  return res.status(200).json(res.locals.delete);
+});
+
+module.exports = router;
