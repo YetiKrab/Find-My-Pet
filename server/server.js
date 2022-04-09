8d68f82cf7b634bd840cb7201ee69b155dd823e5
Celeste Knopf <Celeste@Celestes-Air.home>
2022-04-09 11:27:26 -0400

    routes and server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,46 @@
-//this is a test
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+//require routers
+const petRouter = require('./routes/petRoute.js');
+const apiRouter = require('./routes/api.js');
+
+// parse incoming request body
+app.use(express.json());
+
+// express static
+
+// route handlers
+// app.use('/')
+
+// route handler to respond with main app
+app.get('/', (req, res) => {
+  return res
+    .status(200)
+    .sendFile(path.resolve(__dirname, '../client/index.html'));
+});
+
+// route handler for any requests to unknown route
+app.use((req, res) => res.sendStatus(404));
+
+// express global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = app;
